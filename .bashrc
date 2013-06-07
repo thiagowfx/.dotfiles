3851df58e0999143bc8c5a3572e97f3086f2f2f5
thiagowfx <thiagoperrotta95@gee-mail.com>
2013-06-07 13:47:28 -0300

    minor changes

--- .bashrc
+++ .bashrc
@@ -1,3 +1,7 @@
+if [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+fi
+
 ## user aliases
 
 # easy updating command
--- ubuntu-post-installation.sh
+++ ubuntu-post-installation.sh
@@ -1,6 +1,6 @@
 sudo aptitude -y update && sudo aptitude -y upgrade
+sudo aptitude remove unity-lens-shopping
 
-sudo aptitude install calibre classic-menu-indicator anki emacs emacs-goodies-el gparted p7zip guake p7zip-rar virtualbox tomboy nfoview unison-gtk unison krename chmsee bluefish kdiff3 geogebra carmetal kolourpaint4 xchat ttf-mscorefonts-installer winff pingus inkscape blender kile gwenview gimp lyx gnucash gnucash retext pdfsam calligraflow deluge wxmaxima clementine audacity vlc smplayer easytag gpodder avidemux kdenlive winff vim mtpfs mtp-tools dropbox g++ openjdk-7-jre eclipse git git-core apt-file
-dropbox compizconfig-settings-manager unity-tweak-tool skype scribus gnome-tweak-tool mozplugger gedit-plugins
+sudo aptitude install calibre anki emacs emacs-goodies-el gparted p7zip guake p7zip-rar virtualbox tomboy nfoview unison-gtk unison krename chmsee bluefish kdiff3 geogebra carmetal kolourpaint4 xchat ttf-mscorefonts-installer winff pingus inkscape blender kile gwenview gimp lyx gnucash gnucash retext pdfsam calligraflow deluge wxmaxima clementine audacity vlc smplayer easytag gpodder avidemux kdenlive winff vim mtpfs mtp-tools dropbox g++ openjdk-7-jre eclipse git git-core apt-file dropbox compizconfig-settings-manager unity-tweak-tool skype scribus gnome-tweak-tool mozplugger gedit-plugins clipit w3m autocomplete youtube-dl ubuntu-restricted-extras
 
-sudo aptitude -y autoremove && sudo apt-get -y autoclean
+sudo apt-get -y autoremove >>/dev/null && sudo apt-get -y autoclean >>/dev/null
