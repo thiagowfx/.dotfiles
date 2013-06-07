b3506b3bdbd93c15b17e551c9ca2814bfa4d7f1a
Thiago Barroso Perrotta <thiagoperrotta95@gmail.com>
2013-06-07 12:33:57 -0300

    added ubuntu script

---
+++ ubuntu-post-installation.sh
@@ -0,0 +1,6 @@
+sudo aptitude -y update && sudo aptitude -y upgrade
+
+sudo aptitude install calibre classic-menu-indicator anki emacs emacs-goodies-el gparted p7zip guake p7zip-rar virtualbox tomboy nfoview unison-gtk unison krename chmsee bluefish kdiff3 geogebra carmetal kolourpaint4 xchat ttf-mscorefonts-installer winff pingus inkscape blender kile gwenview gimp lyx gnucash gnucash retext pdfsam calligraflow deluge wxmaxima clementine audacity vlc smplayer easytag gpodder avidemux kdenlive winff vim mtpfs mtp-tools dropbox g++ openjdk-7-jre eclipse git git-core apt-file
+dropbox compizconfig-settings-manager unity-tweak-tool
+
+sudo aptitude -y autoremove && sudo aptitude -y autoclean
