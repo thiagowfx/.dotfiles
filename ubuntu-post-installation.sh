1ffa94357c23555dc06cd4ab53c59eb63e079d00
thiagowfx <thiagoperrotta95@gee-mail.com>
2013-06-07 16:33:14 -0300

    some major changes

--- ubuntu-post-installation.sh
+++ ubuntu-post-installation.sh
@@ -1,6 +1,24 @@
+
+# First, let's get aptitude, which is a little better than apt-get
+sudo apt-get -y install aptitude
+
+# Then, let's do a little refreshing
 sudo aptitude -y update && sudo aptitude -y upgrade
-sudo aptitude remove unity-lens-shopping
+echo "Repos updated and upgraded."
 
-sudo aptitude install calibre anki emacs emacs-goodies-el gparted p7zip guake p7zip-rar virtualbox tomboy nfoview unison-gtk unison krename chmsee bluefish kdiff3 geogebra carmetal kolourpaint4 xchat ttf-mscorefonts-installer winff pingus inkscape blender kile gwenview gimp lyx gnucash gnucash retext pdfsam calligraflow deluge wxmaxima clementine audacity vlc smplayer easytag gpodder avidemux kdenlive winff vim mtpfs mtp-tools dropbox g++ openjdk-7-jre eclipse git git-core apt-file dropbox compizconfig-settings-manager unity-tweak-tool skype scribus gnome-tweak-tool mozplugger gedit-plugins clipit w3m autocomplete youtube-dl ubuntu-restricted-extras
+# Remove unnecessary programs
+sudo aptitude remove -y unity-lens-shopping rhythmbox evolution
 
+# Install several programs, asking for the user confirmation (security issues)
+sudo aptitude install calibre anki emacs emacs-goodies-el gparted p7zip guake p7zip-rar virtualbox tomboy nfoview unison-gtk unison krename chmsee bluefish kdiff3 geogebra carmetal kolourpaint4 xchat ttf-mscorefonts-installer winff pingus inkscape blender kile gwenview gimp lyx gnucash gnucash retext pdfsam calligraflow deluge wxmaxima clementine audacity vlc smplayer easytag gpodder avidemux kdenlive winff vim mtpfs mtp-tools dropbox g++ openjdk-7-jre eclipse git git-core apt-file dropbox compizconfig-settings-manager unity-tweak-tool skype scribus gnome-tweak-tool mozplugger gedit-plugins clipit w3m youtube-dl ubuntu-restricted-extras racket wine winetricks playonlinux bleachbit flashplugin-installer gsfonts-x11 indicator-multiload libavformat-extra-53 libavcodec-extra-53 w32-codecs non-free-codecs dia radiotray
+echo "Programs installed."
+
+# Install some programs provided by PPAs
+# sudo aptitude install zramswap-enabler oracle-jdk7-installer y-ppa-manager
+
+# Install proprietary drivers
+# sudo aptitude install nvidia-current nvidia-settings
+
+# Clean the cache, hiding unwanted messages
 sudo apt-get -y autoremove >>/dev/null && sudo apt-get -y autoclean >>/dev/null
+echo "Cache cleaned."
---
+++ ubuntu-ppas.sh
@@ -0,0 +1,25 @@
+# VLC Media Player
+sudo add-apt-repository ppa:videolan/stable-daily
+
+# Deluge BitTorrent Client
+sudo add-apt-repository ppa:deluge-team/ppa
+
+# Oracle Java
+sudo add-apt-repository ppa:webupd8team/java
+
+# Wine
+sudo add-apt-repository ppa:ubuntu-wine/ppa
+
+# Mozilla Thunderbird
+sudo add-apt-repository ppa:mozillateam/thunderbird-stable
+
+# Y PPA Manager
+sudo add-apt-repository ppa:webupd8team/y-ppa-manager
+
+# Indicator Multiload
+sudo add-apt-repository ppa:indicator-multiload/stable-daily
+
+#sudo add-apt-repository ppa:pidgin-developers/ppa
+#sudo add-apt-repository ppa:nilarimogard/webupd8 
+#sudo add-apt-repository ppa:gnome3-team/gnome3
+#sudo add-apt-repository ppa:shnatsel/zram
