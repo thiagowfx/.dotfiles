f663c4d9c7b77d39e0a22e39f72ac87eda00f781
thiagowfx <thiagoperrotta95@gee-mail.com>
2013-06-10 22:21:56 -0300

    some major changes

--- .bashrc
+++ .bashrc
@@ -1,8 +1,15 @@
+# enable bash autocompletion feature
 if [ -f /etc/bash_completion ]; then
     . /etc/bash_completion
 fi
 
 ## user aliases
+
+# add to PATH
+export PATH="/opt/TeamSpeak3-Client-linux_x86/:$PATH"
+
+# default EDITOR. Other options: vim, gvim, nano
+export EDITOR="emacs -nw"
 
 # easy updating command
 alias upq="sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade && sudo apt-get -y autoclean && sudo apt-get -y autoremove"
@@ -11,12 +18,23 @@ alias upq="sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y 
 # openSUSE
 # alias upq="sudo zypper -n update && sudo zypper -n upgrade && sudo -n zypper dup"
 
-# force opening emacs in terminal (not the X client) while in terminal
-alias emacs="emacs -nw"
+# force the usage of other programs. Use with caution!
+alias top="htop"
+alias vi="vim"
+alias emacs="emacs -nw" # force opening emacs in terminal (not the X client) while in terminal
 
 # utilities for bash
 alias l="ls -al"
 alias la="ls -a"
 alias ll="ls -l"
+alias sai="sudo aptitude install"
+alias sar="sudo aptitude remove"
+alias sash="sudo aptitude show"
+alias sase="sudo aptitude search"
 alias youtube-dl-mp3-download="youtube-dl -t --extract-audio --audio-format mp3"
 alias youtuble-dl-video-download="youtube-dl -t"
+
+# windows alias
+alias cls="echo Using Unix program clear; clear"
+alias tracert="echo Using Unix program traceroute; traceroute"
+alias ipconfig="echo Using Unix program ifconfig; ifconfig"
--- ubuntu-post-installation.sh
+++ ubuntu-post-installation.sh
@@ -1,4 +1,3 @@
-
 # First, let's get aptitude, which is a little better than apt-get
 sudo apt-get -y install aptitude
 
@@ -7,11 +6,21 @@ sudo aptitude -y update && sudo aptitude -y upgrade
 echo "Repos updated and upgraded."
 
 # Remove unnecessary programs
-sudo aptitude remove -y unity-lens-shopping rhythmbox evolution
+sudo aptitude remove -y unity-lens-shopping rhythmbox evolution totem transmission-gtk transmission-common
 
-# Install several programs, asking for the user confirmation (security issues)
-sudo aptitude install calibre anki emacs emacs-goodies-el gparted p7zip guake p7zip-rar virtualbox tomboy nfoview unison-gtk unison krename chmsee bluefish kdiff3 geogebra carmetal kolourpaint4 xchat ttf-mscorefonts-installer winff pingus inkscape blender kile gwenview gimp lyx gnucash gnucash retext pdfsam calligraflow deluge wxmaxima clementine audacity vlc smplayer easytag gpodder avidemux kdenlive winff vim mtpfs mtp-tools dropbox g++ openjdk-7-jre eclipse git git-core apt-file dropbox compizconfig-settings-manager unity-tweak-tool skype scribus gnome-tweak-tool mozplugger gedit-plugins clipit w3m youtube-dl ubuntu-restricted-extras racket wine winetricks playonlinux bleachbit flashplugin-installer gsfonts-x11 indicator-multiload libavformat-extra-53 libavcodec-extra-53 w32-codecs non-free-codecs dia radiotray
+## Install several programs, asking for the user confirmation (security issues)
+sudo aptitude install calibre anki emacs emacs-goodies-el gparted p7zip guake p7zip-rar virtualbox tomboy nfoview unison-gtk unison krename chmsee bluefish kdiff3 geogebra carmetal kolourpaint4 xchat ttf-mscorefonts-installer winff pingus inkscape blender kile gwenview gimp lyx gnucash gnucash retext pdfsam calligraflow deluge wxmaxima clementine audacity vlc smplayer easytag gpodder avidemux kdenlive winff vim mtpfs mtp-tools dropbox g++ openjdk-7-jre eclipse git git-core apt-file dropbox compizconfig-settings-manager unity-tweak-tool skype scribus gnome-tweak-tool mozplugger gedit-plugins clipit w3m youtube-dl ubuntu-restricted-extras racket wine winetricks playonlinux bleachbit flashplugin-installer gsfonts-x11 indicator-multiload libavformat-extra-53 libavcodec-extra-53 w32-codecs non-free-codecs dia radiotray scilab nautilus-open-terminal unity-2d indent markdown python-progressbar idle openshot
+
+# images
+sudo aptitude install feh                   # gpicview
+# network utilities
+sudo aptitude install traceroute nmap
+# shell
+sudo aptitude install ranger                # file manager
 echo "Programs installed."
+
+## Others
+# sc # spreadsheet in terminal
 
 # Install some programs provided by PPAs
 # sudo aptitude install zramswap-enabler oracle-jdk7-installer y-ppa-manager
