1f0aa49744a420063d881a6a74a420e76f50f7c7
thiagowfx <thiagoperrotta95@gee-mail.com>
2013-06-11 09:28:46 -0300

    minor changes

--- ubuntu-ppas.sh
+++ ubuntu-ppas.sh
@@ -19,6 +19,9 @@ sudo add-apt-repository ppa:webupd8team/y-ppa-manager
 # Indicator Multiload
 sudo add-apt-repository ppa:indicator-multiload/stable-daily
 
+# Kubuntu Backports (caution here!)
+sudo add-apt repository ppa:kubuntu-ppa/backports
+
 #sudo add-apt-repository ppa:pidgin-developers/ppa
 #sudo add-apt-repository ppa:nilarimogard/webupd8 
 #sudo add-apt-repository ppa:gnome3-team/gnome3
