73377c9b88caae767dba9fe65acf97a1904f8756
thiagowfx <thiagoperrotta95@gmail.com>
2013-06-14 11:57:40 -0300

    minor changes

--- ubuntu-post-installation.sh
+++ ubuntu-post-installation.sh
@@ -1,5 +1,6 @@
 # First, let's get aptitude, which is a little better than apt-get
 sudo apt-get -y install aptitude
+echo "aptitude package manager installed".
 
 # Then, let's do a little refreshing
 sudo aptitude -y update && sudo aptitude -y upgrade
@@ -7,9 +8,11 @@ echo "Repos updated and upgraded."
 
 # Remove unnecessary programs
 sudo aptitude remove -y unity-lens-shopping rhythmbox evolution totem transmission-gtk transmission-common
+echo "Unnecessary programs removed."
 
 ## Install several programs, asking for the user confirmation (security issues)
-sudo aptitude install zram-config calibre anki emacs emacs-goodies-el gparted p7zip guake p7zip-rar virtualbox tomboy nfoview unison-gtk unison krename chmsee bluefish kdiff3 geogebra carmetal kolourpaint4 xchat ttf-mscorefonts-installer winff pingus inkscape blender kile gwenview gimp lyx gnucash gnucash retext pdfsam calligraflow deluge wxmaxima clementine audacity vlc smplayer easytag gpodder avidemux kdenlive winff vim mtpfs mtp-tools dropbox g++ openjdk-7-jre eclipse git git-core apt-file dropbox compizconfig-settings-manager unity-tweak-tool skype scribus gnome-tweak-tool mozplugger gedit-plugins clipit w3m youtube-dl ubuntu-restricted-extras racket wine winetricks playonlinux bleachbit flashplugin-installer gsfonts-x11 indicator-multiload libavformat-extra-53 libavcodec-extra-53 w32-codecs non-free-codecs dia radiotray scilab nautilus-open-terminal unity-2d indent markdown python-progressbar idle openshot xpaint kipi-plugins
+sudo aptitude install zram-config calibre anki emacs emacs-goodies-el gparted p7zip guake p7zip-rar virtualbox tomboy nfoview unison-gtk unison krename chmsee bluefish kdiff3 geogebra carmetal kolourpaint4 xchat ttf-mscorefonts-installer winff pingus inkscape blender kile gwenview gimp lyx gnucash gnucash retext pdfsam calligraflow deluge wxmaxima clementine audacity vlc smplayer easytag gpodder avidemux kdenlive winff vim mtpfs mtp-tools dropbox g++ openjdk-7-jre eclipse git git-core apt-file dropbox compizconfig-settings-manager unity-tweak-tool skype scribus gnome-tweak-tool mozplugger gedit-plugins clipit w3m youtube-dl ubuntu-restricted-extras racket wine winetricks playonlinux bleachbit flashplugin-installer gsfonts-x11 indicator-multiload libavformat-extra-53 libavcodec-extra-53 w32-codecs non-free-codecs dia radiotray scilab nautilus-open-terminal unity-2d indent markdown python-progressbar idle openshot xpaint kipi-plugins unity-webapps-googlecalendar
+echo "Several programs installed".
 
 # general shell utilities
 sudo aptitude install -y htop
