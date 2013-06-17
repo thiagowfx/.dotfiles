40308ce93f4867ff5f233bf340d83bca415df974
thiagowfx <thiagoperrotta95@gmail.com>
2013-06-16 23:39:18 -0300

    removed thunar

--- arch-post-installation.sh
+++ arch-post-installation.sh
@@ -4,7 +4,9 @@ sudo pacman -Syu
 
 # Packages to install. I'm trying to be minimalist here.
 sudo pacman -S w3m emacs-nox git fluxbox xterm feh zathura 
-ranger texlive-bin racket ranger zsh tomboy gparted wget thunar thunar-volman
+ranger texlive-bin racket ranger zsh tomboy gparted wget
+
+# thunar thunar-volman
 
 # Alterar mirrorlist ==> colocar os servidores brasileiros lá em cima, senão ficará preso em 50kb/s.
 sudo emacs -nw /etc/pacman.d/mirrorlist
