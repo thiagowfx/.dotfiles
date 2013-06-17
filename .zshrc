23b5fdcc93d077ebd09cdc1e01270b0bb5a801de
thiagowfx <thiagoperrotta95@gmail.com>
2013-06-16 23:38:12 -0300

    some minor changes

---
+++ .zshrc
@@ -0,0 +1,13 @@
+# Lines configured by zsh-newuser-install
+HISTFILE=~/.histfile
+HISTSIZE=1000
+SAVEHIST=1000
+setopt beep
+bindkey -e
+# End of lines configured by zsh-newuser-install
+# The following lines were added by compinstall
+zstyle :compinstall filename '/home/thiago/.zshrc'
+
+autoload -Uz compinit
+compinit
+# End of lines added by compinstall
--- arch-post-installation.sh
+++ arch-post-installation.sh
@@ -3,8 +3,8 @@ sudo pacman -Syy
 sudo pacman -Syu
 
 # Packages to install. I'm trying to be minimalist here.
-sudo pacman -S w3m emacs-nox git fluxbox xterm pcmanfm feh zathura 
-ranger texlive-bin racket ranger zsh tomboy gparted wget 
+sudo pacman -S w3m emacs-nox git fluxbox xterm feh zathura 
+ranger texlive-bin racket ranger zsh tomboy gparted wget thunar thunar-volman
 
 # Alterar mirrorlist ==> colocar os servidores brasileiros lá em cima, senão ficará preso em 50kb/s.
 sudo emacs -nw /etc/pacman.d/mirrorlist
