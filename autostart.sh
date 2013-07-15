0583f962d65559c849696df9928d247b6f470237
thiagowfx <thiagoperrotta95@gmail.com>
2013-07-14 23:19:08 -0300

    general update

---
+++ autostart.sh
@@ -0,0 +1,10 @@
+# touchpad tweaking
+synclient TapButton1=1
+synclient TapButton2=3
+synclient TapButton3=2
+synclient VertEdgeScroll=1
+synclient VertTwoFingerScroll=1
+synclient HorizTwoFingerScroll=1
+synclient PalmDetect=1
+synclient PalmMinWidth=10
+synclient PalmMinZ=200
