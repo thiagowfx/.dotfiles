6e890256a8b006e63c7a4fda528c507246981d1e
thiagowfx <thiagoperrotta95@gmail.com>
2013-07-25 21:03:32 -0300

    some changes

--- .zshrc
+++ .zshrc
@@ -79,7 +79,7 @@ alias ls='ls --group-directories-first --time-style=+"%d.%m.%Y %H:%M" --color=au
 alias ll='ls -l --group-directories-first --time-style=+"%d.%m.%Y %H:%M" --color=auto -F'
 alias ln="ln -v"
 alias mv="mv -v"
-alias pgrep="pgrep --color=auto"
+alias pgrep="pgrep --list-name"
 alias rm="rm -v -i"
 alias vi="vim"
 
@@ -89,8 +89,8 @@ alias vi="vim"
 alias mount-iso="mount -o loop"
 
 # require youtube-dl package
-alias youtube-dl-mp3-download="youtube-dl -t --extract-audio --audio-format mp3"
-alias youtube-dl-video-download="youtube-dl -t"
+alias youtube-dl-mp3-download="youtube-dl -t --restrict-filenames --extract-audio --audio-format mp3"
+alias youtube-dl-video-download="youtube-dl -t --restrict-filenames"
 
 # most used commands
 alias top10="fc -l 0 | awk '{print \$2}' | awk 'BEGIN {FS=\"|\"} {print \$1}' | sort | uniq -c | sort -rn | head -n 10"
--- misc/install.sh
+++ misc/install.sh
@@ -3,30 +3,63 @@
 # Based on http://pastebin.com/kwVN3QQH
 
 ########################################
-# Variables - customize here
+# variables - customize here
 ########################################
 
 dir=~/dotfiles		# dotfiles directory
 olddir=~/.dotfiles_backup	# old dotfiles backup directory
 vundledir=~/.vim/bundle/vundle
 files=".bashrc .conkyrc .emacs .gitconfig .gitignore .vimrc .yaourtrc .zshrc" # list of files/folders to symlink in homedir
+musicdir=~/Copy/Music
+picturesdir=~/Dropbox/Pictures
+videosdir=~/Downloads/Torrents
+templatesdir=~/Copy/Templates
 
 ########################################
+# create custom links for dotfiles
+########################################
 
-# create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
+# create dotfiles_backup in homedir
 mkdir -p $olddir
-echo "...done"
 
 # change to the dotfiles directory
-echo "Changing to the $dir directory"
 cd $dir
-echo "...done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+# move any existing dotfiles in homedir to dotfiles_backup directory, then create symlinks 
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/$file ~/dotfiles_old/
+    mv ~/$file $olddir
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/$file
 done
+
+########################################
+# create symlinks for folders
+########################################
+if [[ -f $musicdir ]]; then
+    touch .dummy
+else
+    mkdir -p $musicdir
+    ln -s $musicdir ~/Music
+fi
+
+if [[ -f $picturesdir ]]; then
+    touch .dummy
+else
+    mkdir -p $picturesdir
+    ln -s $picturesdir ~/Pictures
+fi
+
+if [[ -f $videosdir ]]; then
+    touch .dummy
+else
+    mkdir -p $videosdir
+    ln -s $videosdir ~/Videos
+fi
+
+if [[ -f $templatesdir ]]; then
+    touch .dummy
+else
+    mkdir -p $templatesdir
+    ln -s $templatesdir ~/Templates
+fi
