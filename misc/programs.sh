8d319374a94f79575e89630ff5149eb0347209cf
thiagowfx <thiagoperrotta95@gmail.com>
2013-08-27 17:57:48 -0300

    changed wget

--- misc/programs.sh
+++ misc/programs.sh
@@ -1,3 +1,6 @@
 # VLSub: VLC extension to download subtitles
 mkdir -p ~/.local/share/vlc/lua/extensions && cd $_
 wget https://raw.github.com/exebetche/vlsub/master/vlsub.lua
+
+# set default file manager (ex. for Dropbox)
+xdg-mime default Thunar.desktop inode/directory
