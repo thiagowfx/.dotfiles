48185deaeb4c386e7453bd2a6d925e02d5c4dbcb
thiagowfx <thiagoperrotta95@gmail.com>
2013-10-03 10:40:34 -0300

    added 2 files

---
+++ misc/ppas.sh
@@ -0,0 +1,5 @@
+ppas="ppa:videolan/stable-daily ppa:me-davidsansome/clementine ppa:mozillateam/thunderbird-stable ppa:ubuntu-wine/ppa ppa:webudp8team/y-ppa-manager ppa:danielrichter2007/grub-customizer ppa:noobslab/mint ppa:lyx-devel/daily ppa:libreoffice/ppa ppa:elementary-os/daily"
+
+for ppa in $ppas; do
+    sudo add-apt-repository $ppa
+done
