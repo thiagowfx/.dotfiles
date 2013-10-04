4e9c5302ebeab0f67261c23d8dfb80db133158fa
thiagowfx <thiagoperrotta95@gmail.com>
2013-10-03 23:10:09 -0300

    added calibre

--- misc/programs.sh
+++ misc/programs.sh
@@ -2,6 +2,9 @@
 mkdir -p ~/.local/share/vlc/lua/extensions && cd $_
 wget https://raw.github.com/exebetche/vlsub/master/vlsub.lua
 
+# Calibre Download / Update
+sudo python -c "import sys; py3 = sys.version_info[0] > 2; u = __import__('urllib.request' if py3 else 'urllib', fromlist=1); exec(u.urlopen('http://status.calibre-ebook.com/linux_installer').read()); main()"
+
 # set default file manager (ex. for Dropbox)
 # xdg-mime default Thunar.desktop inode/directory
 # xdg-mime default Nemo.desktop inode/directory
