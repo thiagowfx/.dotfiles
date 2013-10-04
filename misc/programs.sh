2cb2ce09496a47ee7ed3f2208e592fc91c6e351c
thiagowfx <thiagoperrotta95@gmail.com>
2013-10-04 14:37:16 -0300

    Minor tweaks

--- misc/programs.sh
+++ misc/programs.sh
@@ -5,9 +5,9 @@ wget https://raw.github.com/exebetche/vlsub/master/vlsub.lua
 # Calibre Download / Update
 sudo python -c "import sys; py3 = sys.version_info[0] > 2; u = __import__('urllib.request' if py3 else 'urllib', fromlist=1); exec(u.urlopen('http://status.calibre-ebook.com/linux_installer').read()); main()"
 
-# set default file manager (ex. for Dropbox)
-# xdg-mime default Thunar.desktop inode/directory
-# xdg-mime default Nemo.desktop inode/directory
+# Default applications
+xdg-mime default zathura.desktop application/pdf
+xdg-mime default Nemo.desktop inode/directory
 
 # create this script
 # /usr/bin/nemo
