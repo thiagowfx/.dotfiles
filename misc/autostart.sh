97575143873acbf4e1fbda573d69e87ddb48c223
thiagowfx <thiagoperrotta95@gmail.com>
2013-10-11 12:21:22 -0300

    minor changes

---
+++ misc/autostart.sh
@@ -0,0 +1,15 @@
+# touchpad tweaking (synaptics)
+# see also /etc/X11/xorg.conf.d/10-synaptics.conf
+synclient TapButton1=1
+synclient TapButton2=2              # this way 2 fingers = middle button and 3 fingers = right button
+synclient TapButton3=3
+synclient VertEdgeScroll=1
+synclient HorizEdgeScroll=1
+synclient VertTwoFingerScroll=1
+synclient HorizTwoFingerScroll=1
+synclient PalmDetect=1
+synclient PalmMinWidth=10
+synclient PalmMinZ=200
+
+setxkbmap br # set my keyboard to br
+# xbindkeys &
