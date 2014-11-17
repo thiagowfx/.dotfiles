06884354fd9ce0dc0039de68d91ed73185b6e729
Thiago Perrotta <thiagoperrotta95@gmail.com>
2014-11-17 08:34:32 -0200

    add cleanup.sh

---
+++ cleanup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Maintenance script, intended to be run periodically (for example, with cron).
+
+msg() {
+  echo "--> $*..."
+}
+
+msg "Running paccache."
+paccache -r
+
+msg "Running fstrim on the SSD"
+sudo fstrim -v /
+
+msg "Removing orphan packages."
+sudo pacman -Rns $(pacman -Qdtq)
