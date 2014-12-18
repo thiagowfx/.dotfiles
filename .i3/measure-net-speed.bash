4020fc4259c9b39b34e3b532951042233e3e62d8
Alexander Kauer <alexander+git@ax86.net>
2014-12-18 19:16:48 +0100

    Added support for $BLOCK_INSTANCE  and better rounding

--- .i3/measure-net-speed.bash
+++ .i3/measure-net-speed.bash
@@ -4,26 +4,25 @@
 #  people. So here goes: © 2012 Stefan Breunig
 #  stefan+measure-net-speed@mathphys.fsk.uni-heidelberg.de)
 
+if [ -z "$BLOCK_INSTANCE" ]
+  then echo 'please give $BLOCK_INSTANCE'
+  exit 1
+fi
+
+if ! [ -e "/sys/class/net/${BLOCK_INSTANCE}/operstate" ] || ! [ "`cat /sys/class/net/${BLOCK_INSTANCE}/operstate`" = "up" ]
+  then echo ""
+  exit 0
+fi
 
 # path to store the old results in
-path="/dev/shm/measure-net-speed"
+path="/dev/shm/measure-net-speed-$BLOCK_INSTANCE"
 
 # grabbing data for each adapter. 
-# You can find the paths to your adapters using
-#  find /sys/devices -name statistics
-# If you have more (or less) than two adapters, simply adjust the script here
-# and in the next block. 
-enp1s0="/sys/devices/pci0000:00/0000:00:1c.0/0000:01:00.0/net/enp1s0/statistics"
-wlp2s0="/sys/devices/pci0000:00/0000:00:1c.1/0000:02:00.0/net/wlp2s0/statistics"
-read enp1s0_rx < "${enp1s0}/rx_bytes"
-read enp1s0_tx < "${enp1s0}/tx_bytes"
-read wlp2s0_rx < "${wlp2s0}/rx_bytes"
-read wlp2s0_tx < "${wlp2s0}/tx_bytes"
+read rx < "/sys/class/net/${BLOCK_INSTANCE}/statistics/rx_bytes"
+read tx < "/sys/class/net/${BLOCK_INSTANCE}/statistics/tx_bytes"
 
-# get time and sum of rx/tx for combined display
+# get time
 time=$(date +%s)
-rx=$(( $enp1s0_rx + $wlp2s0_rx ))
-tx=$(( $enp1s0_tx + $wlp2s0_tx ))
 
 # write current data if file does not exist. Do not exit, this will cause
 # problems if this file is sourced instead of executed as another process.
@@ -49,14 +48,12 @@ if [[ "${time_diff}" -gt 0 ]]; then
   tx_rate=$(( $tx_diff / $time_diff ))
 
   # shift by 10 bytes to get KiB/s. If the value is larger than
-  # 1024^2 = 1048576, then display MiB/s instead (simply cut off  
-  # the last two digits of KiB/s). Since the values only give an  
-  # rough estimate anyway, this improper rounding is negligible.
+  # 1024^2 = 1048576, then display MiB/s instead
 
   # incoming
   rx_kib=$(( $rx_rate >> 10 ))
   if [[ "$rx_rate" -gt 1048576 ]]; then
-    echo -n "${rx_kib:0:-3}.${rx_kib: -3:-2} M↓"
+    printf '%s M↓' "`echo "scale=1; $rx_kib / 1024" | bc`"
   else
     echo -n "${rx_kib} K↓"
   fi
@@ -66,7 +63,7 @@ if [[ "${time_diff}" -gt 0 ]]; then
   # outgoing
   tx_kib=$(( $tx_rate >> 10 ))
   if [[ "$tx_rate" -gt 1048576 ]]; then
-    echo -n "${tx_kib:0:-3}.${tx_kib: -3:-2} M↑"
+    printf '%s M↑' "`echo "scale=1; $tx_kib / 1024" | bc`"
   else
     echo -n "${tx_kib} K↑"
   fi
