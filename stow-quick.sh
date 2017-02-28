30fe29dfaa344f29c4d350bdf9161ec79af0edfa
Thiago Perrotta <tbperrotta@gmail.com>
2017-02-28 14:56:35 -0300

    add stow-quick.sh and improve scripts in bin
    Former-commit-id: 9e6cb44c824b2952c30e14823c570cb56430f1a8
    Former-commit-id: 0796ab9039e537368bdac375a0c08fbfccbc4cc6
    Former-commit-id: dc4a46c27243605cf2bc2ae53a9af3b676caaed2
    Former-commit-id: e272398df681f369d95393125d883c92d0c9eb48
    Former-commit-id: d2028db6e7aed46e8602ff29c63308123d7b6142 [formerly ea81fdfa8e0019eee44ef6287e1aa64dbe8a43df]
    Former-commit-id: 226d574a74850429fb47a4dedfd3fdede83f84e1

---
+++ stow-quick.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# Quickly stows the most popular stows.
+#
+
+stows=(bash bin git tmux)
+
+while getopts ":u" opt; do
+	case $opt in
+		u)
+			UNSTOW=1
+			;;
+		\?)
+			echo "error: invalid option: -$OPTARG">&2
+			exit 1
+			;;
+	esac
+done
+
+for d in ${stows[@]}; do
+	if [ -z ${UNSTOW} ]; then
+		stow $d
+	else
+		stow -D $d
+	fi
+done
