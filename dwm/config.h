18758cdfc2b1e888410b9dc4ced96b8c245fa4b6
Thiago Perrotta <tbperrotta@gmail.com>
2017-03-01 17:40:10 -0300

    fix dwm config
    Former-commit-id: 087e27ef629993f075b644608a069ef0c1b74845
    Former-commit-id: 03b0efd9cab16fa052afc61d4db2691b9445e794
    Former-commit-id: 3452aa1c0c737d9dbdbd92e1c8a6dc0552dd7ec2
    Former-commit-id: c94b0eaf709663bd1d443f946461dcb3edcc313a
    Former-commit-id: 38206104ccb2cecec72da3a307f244ef9a2bc0cb [formerly d281e9a592f714eab822d878dd78daf9581f8ce6]
    Former-commit-id: a3de8efb8ae2610f134f084b6b48743ad35d677e

--- dwm/config.h
+++ dwm/config.h
@@ -32,13 +32,12 @@ static const unsigned int gappx     = 4; /* gap pixel between windows */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
-	NULL
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	// { "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	// { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
