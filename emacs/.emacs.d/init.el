86c0ca2c2c2d663b078f0081efe16182c4bc2d13
Thiago Perrotta <tbperrotta@gmail.com>
2017-11-27 01:57:55 -0500

    disable flycheck mode in init file
    Former-commit-id: f55a2f0a30ae13afb21cfc560ee435dcfa8cef87
    Former-commit-id: 579e558b64f2b1dfa7aad138f0b93ff53c8e3a45
    Former-commit-id: ff818558f4888978ec530db77a0ef9117743bf26
    Former-commit-id: 931c44773cf5a3ecea2434b99f37fb7a0373cba6
    Former-commit-id: a45ef45ede5383323f7ae9ab770a9deffb0ffcec [formerly 4828de842d4b21d6fa760d1c8c5313cdb7d5e594]
    Former-commit-id: 911a91e2e06e15dcddebec4ad1c77051945f38aa

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -8,3 +8,7 @@
 (setq genesis_corp (expand-file-name "genesis_corp.org" (expand-file-name user-emacs-directory)))
 (when (file-exists-p genesis_corp)
   (org-babel-load-file genesis_corp))
+
+;; Local Variables:
+;; eval: (flycheck-mode -1)
+;; End:
