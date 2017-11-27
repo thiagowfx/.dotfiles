96fca57362e975e0fe36ff2927eb9b22bde47424
Thiago Perrotta <tbperrotta@gmail.com>
2017-11-27 01:49:51 -0500

    add gitconfig_corp
    Former-commit-id: 5ef39933ffdc42b000d24fe7cc38d3783071be55
    Former-commit-id: c1cb24a4741a457b148854f5cafbb754f429ed74
    Former-commit-id: 6dd64ab7cf2512caf7317e212ec0f0b24eb61905
    Former-commit-id: 6af306f6558549380865c2a3465572c5d60ad999
    Former-commit-id: 1b30db6397079f2d6933ce6764e6d90025f042cc [formerly 9250c8c54c13bc36d4503aef60f70524fe2a4305]
    Former-commit-id: 826431542fe69470e1e93989f086ea88a1200de1

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -4,6 +4,7 @@
 
 ;; Load "genesis.org"
 (org-babel-load-file (expand-file-name "genesis.org" (expand-file-name user-emacs-directory)))
+
 (setq genesis_corp (expand-file-name "genesis_corp.org" (expand-file-name user-emacs-directory)))
 (when (file-exists-p genesis_corp)
   (org-babel-load-file genesis_corp))
