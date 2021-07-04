a792e8bc6a18a4c8983e2ec7244f131314aadbc9
Thiago Perrotta <tbperrotta@gmail.com>
2021-07-04 14:16:43 -0400

    use monokai in bash prompt and Xresources
    Former-commit-id: 64ce846235e30d0882946647ecda2b8d0bf90259
    Former-commit-id: 682a00e53d6b9c267f8e410dc42c3c6d96fbfe58
    Former-commit-id: af00acc0f2be19900afca9dde7d92d7f7d8a79d3
    Former-commit-id: 929614f2c262be013758696f106d9df0192e4667
    Former-commit-id: ab5e6df8486464a2788081e0ffc85ce271202172 [formerly 84108c528e86ff58bad9a8b03707f06fc4c2bb0b]
    Former-commit-id: 1e533482333d207fa44730955308b29d466500d8

--- main/.bashrc.d/10-prompt.bash
+++ main/.bashrc.d/10-prompt.bash
@@ -1,12 +1,18 @@
 #!/bin/bash
+# shellcheck disable=SC2034
 # Set bash prompt.
 
-BLUE=$'\e[34;49m'
-GREEN=$'\e[32;49m'
-ORANGE=$'\e[33;49m'
-PURPLE=$'\e[35;49m'
-RED=$'\e[31;49m'
+# Monokai-inspired: https://gist.github.com/transat/6694554
 RESET=$'\e[0m'
+BOLD=$'\e[1m'
+RED=$'\e[31;40m'
+BLUE=$'\e[34;40m'
+LIGHTBLUE=$'\e[36;40m'
+GREEN=$'\e[32;40m'
+ORANGE=$'\e[33;40m'
+PINK=$'\e[31;40m'
+PURPLE=$'\e[35;40m'
+YELLOW=$'\e[37;40m'
 
 prompt_command() {
 	# this must be the first line
@@ -22,10 +28,10 @@ prompt_command() {
 	[ $EXIT != 0 ] && PS1+="\\[$RED\\]$EXIT "
 
 	# add user, hostname and directory
-	PS1+='\[$GREEN\]\u@\h \[$BLUE\]\w'
+	PS1+='\[$BLUE\]\u\[$RESET\]@\h \[$BOLD\]\w\[$RESET\]'
 
 	# add vanilla git prompt if existing
-	hash __git_ps1 &>/dev/null && PS1+="\\[$PURPLE\\]\$(__git_ps1)"
+	hash __git_ps1 &>/dev/null && PS1+="\\[$GREEN\\]\$(__git_ps1)"
 
 	# add prompt
 	PS1+='\n\[$ORANGE\]>> \[$RESET\]'
