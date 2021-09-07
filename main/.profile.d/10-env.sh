7b1a7152301cf9ee5b8f9fa2229a2b46b49a60f1
Thiago Perrotta <tbperrotta@gmail.com>
2021-09-07 01:45:50 -0400

    man: colored pages in both arch and debian

--- main/.profile.d/10-env.sh
+++ main/.profile.d/10-env.sh
@@ -17,12 +17,25 @@ add_env VISUAL "vim" vim
 #   --raw-control-chars --ignore-case
 add_env LESS "-R -i" less
 
-# Display progress percentage in man pages: https://unix.stackexchange.com/a/329092/41858
+# Enhanced man pages
+#   Color: https://wiki.archlinux.org/title/Color_output_in_console#man
+#   Progress percentage: https://unix.stackexchange.com/a/329092/41858
 add_env MANPAGER "less -R -s -M +Gg" less
 
-# Colored man pages: https://wiki.archlinux.org/title/Color_output_in_console#man
-if hash less >/dev/null 2>&1 && [ "$(less -V | head -n 1 | cut -f2 -d' ')" -ge 580 ]; then
-	add_env MANPAGER "less -R -s -M +Gg --use-color -Dd+r -Du+b" less
+if hash less >/dev/null 2>&1; then
+	if [ "$(less -V | head -n 1 | cut -f2 -d' ')" -ge 580 ]; then
+		add_env MANPAGER "${MANPAGER} --use-color -Dd+r -Du+b"
+	else
+		man() {
+			LESS_TERMCAP_md=$'\e[01;31m' \
+				LESS_TERMCAP_me=$'\e[0m' \
+				LESS_TERMCAP_se=$'\e[0m' \
+				LESS_TERMCAP_so=$'\e[01;44;33m' \
+				LESS_TERMCAP_ue=$'\e[0m' \
+				LESS_TERMCAP_us=$'\e[01;34m' \
+				command man "$@"
+			}
+	fi
 fi
 
 if hash fzf >/dev/null 2>&1; then
