a2400f91bb10ca58185d56599c944474864ffbf8
Thiago Perrotta <tbperrotta@gmail.com>
2021-09-06 22:12:30 -0400

    zsh: use built-in commands array instead of hash

--- main/.zshrc.d/20-external.zsh
+++ main/.zshrc.d/20-external.zsh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 # fzf: Fuzzy File Finder
-if hash fzf >/dev/null 2>&1; then
+if (( $+commands[fzf] )); then
 	# fzf config - arch
 	src_files /usr/share/fzf/{completion,key-bindings}.zsh
 
@@ -11,7 +11,7 @@ if hash fzf >/dev/null 2>&1; then
 fi
 
 # pacman from Arch Linux
-if hash pacman >/dev/null 2>&1; then
+if (( $+commands[pacman] )); then
 	# command-not-found hook
 	src_files "/usr/share/doc/pkgfile/command-not-found.zsh"
 fi
