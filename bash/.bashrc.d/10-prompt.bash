7562259b54ef40d76f0730ba455d9da2320bcb97
Thiago Perrotta <tbperrotta@gmail.com>
2021-12-25 19:19:00 -0500

    bash prompt: make root user red

--- bash/.bashrc.d/10-prompt.bash
+++ bash/.bashrc.d/10-prompt.bash
@@ -14,7 +14,7 @@ PINK=$'\e[31;49m'
 PURPLE=$'\e[35;49m'
 YELLOW=$'\e[37;49m'
 
-# git prompt customization: https://wiki.archlinux.org/title/Git
+# __git_ps1 prompt customization: https://wiki.archlinux.org/title/Git
 if hash git >/dev/null 2>&1; then
 	GIT_PS1_SHOWDIRTYSTATE=1
 
@@ -38,8 +38,15 @@ prompt_command() {
 	# add exit code from previous command if unsuccessful
 	[ $EXIT != 0 ] && PS1+="\\[$BOLD\\]\\[$RED\\]$EXIT "
 
-	# add user, hostname and directory
-	PS1+='\[$BOLD\]\[$BLUE\]\u\[$RESET\]@\h \[$BOLD\]\w\[$RESET\]'
+	# add user, root is red
+	if [ "$(id -u)" -eq 0 ]; then
+		PS1+='\[$BOLD\]\[$RED\]\u'
+	else
+		PS1+='\[$BOLD\]\[$BLUE\]\u'
+	fi
+
+	# add hostname and directory
+	PS1+='\[$RESET\]@\h \[$BOLD\]\w\[$RESET\]'
 
 	# add vanilla git prompt if existing
 	hash __git_ps1 &>/dev/null && PS1+="\\[$GREEN\\]\$(__git_ps1)"
