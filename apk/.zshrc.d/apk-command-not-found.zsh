dcbfbc2e172b49eb95d659838d03606e849829ab
Thiago Perrotta <tbperrotta@gmail.com>
2021-12-25 19:11:44 -0500

    apk: add command_not_found_handler for alpine linux

---
+++ apk/.zshrc.d/apk-command-not-found.zsh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+# apk(8) from Alpine Linux command not found hook
+
+command_not_found_handler() {
+	local cmd="$1"
+	local pkgs=(${(f)"$(apk list -P -- "cmd:$cmd" 2>/dev/null)"})
+
+	if [[ -n "$pkgs" ]]; then
+		echo "$cmd may be found in the following packages:"
+		echo "  $pkgs[@]"
+	else
+		echo "zsh: command not found: $cmd"
+	fi 1>&2
+
+	return 127
+}
