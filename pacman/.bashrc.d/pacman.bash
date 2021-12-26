acd7d7946469cb01c7733f8f565b1c99210b029a
Thiago Perrotta <tbperrotta@gmail.com>
2021-12-25 21:18:51 -0500

    pacman: extract to separate file

--- pacman/.bashrc.d/pacman.bash
+++ pacman/.bashrc.d/pacman.bash
@@ -5,3 +5,4 @@ if hash pacman >/dev/null 2>&1; then
 	# command-not-found hook
 	src_files "/usr/share/doc/pkgfile/command-not-found.bash"
 fi
+
--- pacman/.zshrc.d/pacman.zsh
+++ pacman/.zshrc.d/pacman.zsh
@@ -5,3 +5,4 @@ if (( $+commands[pacman] )); then
 	# command-not-found hook
 	src_files "/usr/share/doc/pkgfile/command-not-found.zsh"
 fi
+
