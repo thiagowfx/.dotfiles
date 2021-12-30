90d1b06af44ab582bf7086725d4b0b6946d782f9
Thiago Perrotta <tbperrotta@gmail.com>
2021-12-29 21:41:01 -0500

    remove unused zsh constructs

--- zsh/.zshrc.d/plugins.zsh
+++ zsh/.zshrc.d/plugins.zsh
@@ -5,10 +5,8 @@ src_files "$HOME/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
 
 # Fish shell-like autosuggestions for zsh.
 # Suggest commands as you type based on history and completions.
-#   Default: history only.
 #   Use C-e or C-f to accept a suggestion.
 src_files "$HOME/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh"
-ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 
 # Fish shell-like history search (up arrow).
 # Load it after zsh-syntax-highlighting.
