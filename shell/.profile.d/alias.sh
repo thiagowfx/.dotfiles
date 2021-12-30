d1e25eebee1dcc38ebdcc3a78e520788e7d87e64
Thiago Perrotta <tbperrotta@gmail.com>
2021-12-29 21:30:30 -0500

    s/add_alias/set_alias

--- shell/.profile.d/alias.sh
+++ shell/.profile.d/alias.sh
@@ -1,21 +1,22 @@
 # Sensible command defaults for both bash and zsh.
 
 # color on
-add_alias diff "diff -uN --color=auto" diff
-add_alias grep "grep --color=auto" grep
-add_alias ip "ip --color=auto" ip
+set_alias diff "diff -uN --color=auto" diff
+set_alias grep "grep --color=auto" grep
+set_alias ip "ip --color=auto" ip
 
 # drop-in replacements
-add_alias ls "ls -Fh --color=auto" ls && add_alias ls "exa -F" exa
+set_alias cat "bat" bat
+alias ls="ls -Fh --color=auto" && set_alias ls "exa -F" exa
 
 # misspellings
-add_alias gi git git
-add_alias gt git git
-add_alias sl ls
+set_alias gi git git
+set_alias gt git git
+alias sl=ls
 
 # verbosity++
-add_alias pkgfile "pkgfile -v" pkgfile
+set_alias pkgfile "pkgfile -v" pkgfile
 
 # get interface public ipv4 address
 # https://major.io/icanhazip-com-faq/
-add_alias icanhazip "curl -4 https://icanhazip.com/" curl
+set_alias icanhazip "curl -4 https://icanhazip.com/" curl
