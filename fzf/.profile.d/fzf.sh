898d4409033f129484c7591597f9a8b511255279
Thiago Perrotta <tbperrotta@gmail.com>
2021-12-29 22:40:18 -0500

    fzf: remove unused M-c

--- fzf/.profile.d/fzf.sh
+++ fzf/.profile.d/fzf.sh
@@ -5,6 +5,5 @@ if hash fzf >/dev/null 2>&1; then
 	# fzf select: C-t
 	export FZF_CTRL_T_OPTS="--preview '(highlight -O ansi {} 2>/dev/null || cat {} || tree -C {}) 2>/dev/null | head -50'"
 
-	# fzf cd: M-c
-	export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -50'"
+	# There's also fzf cd: M-c
 fi
