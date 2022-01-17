4eeb9f4c3c67b3b79d152f2f758c0919f65e7a5c
Thiago Perrotta <tbperrotta@gmail.com>
2022-01-16 21:13:47 -0500

    remove set_env function from shellrc

--- shell/.profile.d/env.sh
+++ shell/.profile.d/env.sh
@@ -7,8 +7,10 @@ path_munge "$HOME/.bin" "$HOME/bin"
 export CLICOLOR=1
 
 # Set preferred text editor.
-set_env EDITOR vim
-set_env VISUAL vim
+if hash vim >/dev/null 2>&1; then
+	export EDITOR="vim"
+	export VISUAL="vim"
+fi
 
 # https://github.com/funtoo/keychain
 hash keychain >/dev/null 2>&1 && eval $(keychain --quiet --eval id_mercury id_water 2&>/dev/null)
