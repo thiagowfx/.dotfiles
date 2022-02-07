d6cb9793d069bc4a637deca462348830fd40b91b
Thiago Perrotta <tbperrotta@gmail.com>
2022-02-07 02:25:52 -0500

    git: add nix support

--- git/.profile.d/git.sh
+++ git/.profile.d/git.sh
@@ -7,8 +7,11 @@ if hash git >/dev/null 2>&1; then
 
 	# debian: https://packages.debian.org/sid/amd64/git/filelist
 	src_files "/usr/lib/git-core/git-sh-prompt"
+
+	# nix
+	src_files "$HOME/.nix-profile/share/git/contrib/completion/git-prompt.sh"
 fi
 
-# git-diff-highlight: arch, debian
+# git-diff-highlight: arch, debian, nix
 # alpine has /usr/bin/diff-highlight out-of-the-box
-path_munge "/usr/share/git/diff-highlight" "/usr/share/doc/git/contrib/diff-highlight"
+path_munge "/usr/share/git/diff-highlight" "/usr/share/doc/git/contrib/diff-highlight" "$HOME/.nix-profile/share/git/contrib/diff-highlight"
