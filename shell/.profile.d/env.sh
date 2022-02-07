02dc62b80cc63d8ddd4e4bfcdda916f76c3f4ae9
Thiago Perrotta <tbperrotta@gmail.com>
2022-02-07 02:39:38 -0500

    shell env: remove keychain

--- shell/.profile.d/env.sh
+++ shell/.profile.d/env.sh
@@ -8,6 +8,3 @@ export CLICOLOR=1
 
 # Set preferred text editor.
 hash vim >/dev/null 2>&1 && export EDITOR="vim" VISUAL="vim"
-
-# https://github.com/funtoo/keychain
-hash keychain >/dev/null 2>&1 && eval $(keychain --quiet --eval id_mercury id_water 2&>/dev/null)
