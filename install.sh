757afcec7cd786bd57b455eb2c48c488546c5848
Thiago Perrotta <tbperrotta@gmail.com>
2022-06-14 15:36:55 +0200

    add install.sh
    for github codespaces: https://docs.github.com/en/codespaces/customizing-your-codespace/personalizing-codespaces-for-your-account#dotfiles

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#
+# Automatically provision dotfiles to github codespaces.
+# https://docs.github.com/en/codespaces/customizing-your-codespace/personalizing-codespaces-for-your-account#dotfiles
+make install
