fabb9cf0ca2410341bd623e429253ca7c6cfc358
Thiago Perrotta <tperrotta@chromium.org>
2022-08-12 20:17:43 +0000

    bash: add gh completion

--- bash/.bashrc.d/plugins.bash
+++ bash/.bashrc.d/plugins.bash
@@ -4,6 +4,9 @@
 # https://direnv.net/
 hash direnv >/dev/null 2>&1 && eval "$(direnv hook bash)"
 
+# gh is github's official command line tool
+hash gh >/dev/null 2>&1 && eval "$(gh completion -s bash)"
+
 # zoxide is a smarter cd command
 # https://github.com/ajeetdsouza/zoxide
 hash zoxide >/dev/null 2>&1 && eval "$(zoxide init bash)"
