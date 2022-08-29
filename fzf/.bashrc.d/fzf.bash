e483f6da4914c25019ba378d2720f98e4c761482
Thiago Perrotta <tperrotta@chromium.org>
2022-08-29 16:27:16 +0200

    fzf: add brew bash
    use of brew --prefix to keep it architecture agnostic

--- fzf/.bashrc.d/fzf.bash
+++ fzf/.bashrc.d/fzf.bash
@@ -4,5 +4,9 @@
 if hash fzf >/dev/null 2>&1; then
 	# alpine/arch, debian
 	src_files {/usr/share/fzf,/usr/share/doc/fzf/examples}/{completion,key-bindings}.bash
+
+	if hash brew >/dev/null 2>&1; then
+		src_files "$(brew --prefix)"/opt/fzf/shell/{completion,key-bindings}.bash
+	fi
 fi
 
