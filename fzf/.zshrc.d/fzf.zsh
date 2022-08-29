25eb8f2fdca36308467a6aa39df7ef42ca408f80
Thiago Perrotta <tperrotta@chromium.org>
2022-08-29 16:18:29 +0200

    fzf: add brew zsh
    use of brew --prefix to keep it architecture agnostic

--- fzf/.zshrc.d/fzf.zsh
+++ fzf/.zshrc.d/fzf.zsh
@@ -4,4 +4,9 @@
 if (( $+commands[fzf] )); then
 	# alpine/arch, debian
 	src_files {/usr/share/fzf,/usr/share/doc/fzf/examples}/{completion,key-bindings}.zsh
+
+	# brew
+	if (( $+commands[brew] )); then
+		src_files "$(brew --prefix)"/opt/fzf/shell/{completion,key-bindings}.zsh
+	fi
 fi
