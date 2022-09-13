502fbf9f9a9ee943ea3693764495a6718fe11b94
Thiago Perrotta <tperrotta@chromium.org>
2022-09-13 23:01:10 +0200

    zsh: make user prompt color green as it's more readable OOTB

--- zsh/.zshrc.d/prompt.zsh
+++ zsh/.zshrc.d/prompt.zsh
@@ -10,5 +10,6 @@ grml_theme_add_token python-venv -f python_venv_prompt '%F{magenta}' '%f'
 # Customize grml prompt.
 #   Two lines instead of one
 #   Add python-venv token
+zstyle ':prompt:grml:left:items:user' pre '%B%F{green}'
 zstyle ':prompt:grml:left:setup' items rc python-venv change-root user at host path vcs newline percent
 
