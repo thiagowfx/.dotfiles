06d63256ed2ac2c5a28594e6f20ccf5c7e5d05b9
Thiago Perrotta <tperrotta@chromium.org>
2023-01-02 15:04:54 +0000

    nix: fix locale error in debian

--- profile/.profile.d/10-nix.sh
+++ profile/.profile.d/10-nix.sh
@@ -1,2 +1,6 @@
+# https://nixos.wiki/wiki/Locales
+# Troubleshooting when using nix on non-NixOS linux distributions
+[[ "$(uname -s)" == "Linux" ]] && export LOCALE_ARCHIVE="/usr/lib/locale/locale-archive"
+
 # https://ariya.io/2020/05/nix-package-manager-on-ubuntu-or-debian
 src_files "$HOME/.nix-profile/etc/profile.d/nix.sh"
