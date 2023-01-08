8d27384e9bd00cc485a19d8524e00989ec41855f
Thiago Perrotta <tbperrotta@gmail.com>
2023-01-08 16:49:13 +0100

    formatting

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-#
 # Automatically provision dotfiles to github codespaces.
 # https://docs.github.com/en/codespaces/customizing-your-codespace/personalizing-codespaces-for-your-account#dotfiles
 make install
