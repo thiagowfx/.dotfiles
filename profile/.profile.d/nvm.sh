71a02978b227e6ff6120f56468bcdfc6452f67f4
Thiago Perrotta <tperrotta@chromium.org>
2023-10-23 22:26:27 +0200

    profile: add nvm

---
+++ profile/.profile.d/nvm.sh
@@ -0,0 +1,5 @@
+# nvm: node version manager
+# https://github.com/nvm-sh/nvm
+# bash_completion is intentional
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" && src_files "$NVM_DIR/bash_completion"
