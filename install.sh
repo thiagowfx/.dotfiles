2e5c7614e0dd29665985bf654b461033755ddc39
Thiago Perrotta <tbperrotta@gmail.com>
2024-03-21 16:23:56 +0100

    remove install.sh (github codespaces)

--- install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Automatically provision dotfiles to github codespaces.
-# https://docs.github.com/en/codespaces/customizing-your-codespace/personalizing-codespaces-for-your-account#dotfiles
-make install
