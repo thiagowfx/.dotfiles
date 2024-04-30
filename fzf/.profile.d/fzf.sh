fd48fecc8d0f37e4a23c357335f03f9d33f8d75c
Thiago Perrotta <tbperrotta@gmail.com>
2024-05-01 00:51:33 +0200

    sd-world + fzf

--- fzf/.profile.d/fzf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if hash fzf >/dev/null 2>&1; then
-	# fzf history: C-r
-	export FZF_CTRL_R_OPTS="--preview-window up:2:wrap --preview 'echo {}'"
-
-	# fzf select: C-t
-	export FZF_CTRL_T_OPTS="--preview '(cat {} || tree -C {}) 2>/dev/null | head -50'"
-fi
