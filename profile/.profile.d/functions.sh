f3435c5741e8a0a69527196a96deb4093a2b9044
Thiago Perrotta <tbperrotta@gmail.com>
2024-09-16 10:59:16 +0200

    add date to cdtmp

--- profile/.profile.d/functions.sh
+++ profile/.profile.d/functions.sh
@@ -3,6 +3,6 @@
 # Inspiration from https://frantic.im/cdtmp/ and grml-zsh-config's cdt
 # Usage: cdtmp [foo]
 cdtmp() {
-	builtin cd "$(mktemp -d "/tmp/$USER-${1:+$1-}XXXXXX")" || return
+	builtin cd "$(mktemp -d "/tmp/$USER-${1:+$1-}$(date +%Y-%m-%d)-XXXXXX")" || return
 	builtin pwd
 }
