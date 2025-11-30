a5e09976f114c8055edd8a1037a51ddeb8c7b372
Thiago Perrotta <thiago@perrotta.dev>
2025-11-30 03:05:36 -0300

    fzf: move to plugins.zsh

--- zsh/.zshrc.d/plugins.zsh
+++ zsh/.zshrc.d/plugins.zsh
@@ -16,12 +16,20 @@ ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 # https://direnv.net/
 (( $+commands[direnv] )) && eval "$(direnv hook zsh)"
 
-# a smarter cd command
+# a smarter cd command - z
 # https://github.com/ajeetdsouza/zoxide
 (( $+commands[zoxide] )) && eval "$(zoxide init zsh)"
 
-# iTerm2 shell integration: https://iterm2.com/documentation-shell-integration.html
-src_files "$HOME/.iterm2_shell_integration.zsh"
+# fzf: fuzzy file finder
+if (( $+commands[fzf] )); then
+        # alpine/arch, debian
+        src_files {/usr/share/fzf,/usr/share/doc/fzf/examples}/{completion,key-bindings}.zsh
+
+        # brew
+        if (( $+commands[brew] )); then
+                src_files "$(brew --prefix)"/opt/fzf/shell/{completion,key-bindings}.zsh
+        fi
+fi
 
 # atuin: https://docs.atuin.sh/
 (( $+commands[atuin] )) && eval "$(atuin init zsh --disable-up-arrow)"
