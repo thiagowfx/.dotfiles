152245264bdc4f55b2a69f13c367bc8dc8ca261d
Thiago Perrotta <thiago@perrotta.dev>
2025-12-21 23:41:34 -0300

    zsh: remove user from prompt

--- zsh/.zshrc.d/prompt.zsh
+++ zsh/.zshrc.d/prompt.zsh
@@ -3,5 +3,4 @@
 
 # Customize grml prompt.
 #   Two lines instead of one
-zstyle ':prompt:grml:left:items:user' pre '%B%F{green}'
-zstyle ':prompt:grml:left:setup' items rc change-root user at host path vcs newline percent
+zstyle ':prompt:grml:left:setup' items rc change-root at host path vcs newline percent
