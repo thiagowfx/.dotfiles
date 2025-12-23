5fc069231a93468b59b1527837a6be39d3b582cc
Thiago Perrotta <thiago@perrotta.dev>
2025-12-22 21:08:55 -0300

    nvim: add gitsigns

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -127,7 +127,27 @@ local plugins = {
   'dense-analysis/ale',
 
   -- Git integration
-  'mhinz/vim-signify',
+  {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup({
+        on_attach = function(bufnr)
+          local gs = require('gitsigns')
+          -- ]c / [c: next/prev hunk (falls back to default in diff mode)
+          vim.keymap.set('n', ']c', function()
+            if vim.wo.diff then return ']c' end
+            vim.schedule(function() gs.next_hunk() end)
+            return '<Ignore>'
+          end, { expr = true, buffer = bufnr, desc = 'Next hunk' })
+          vim.keymap.set('n', '[c', function()
+            if vim.wo.diff then return '[c' end
+            vim.schedule(function() gs.prev_hunk() end)
+            return '<Ignore>'
+          end, { expr = true, buffer = bufnr, desc = 'Prev hunk' })
+        end,
+      })
+    end,
+  },
   'tpope/vim-fugitive',
 
   -- Editing
