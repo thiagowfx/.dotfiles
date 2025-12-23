72ba496969a9d0314b553608f03227ea12c36ed4
Thiago Perrotta <thiago@perrotta.dev>
2025-12-22 21:06:29 -0300

    nvim: better telescope

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -148,7 +148,9 @@ local plugins = {
     'nvim-telescope/telescope.nvim',
     config = function()
       local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
+      vim.keymap.set('n', '<leader>ff', function()
+        builtin.find_files({ hidden = true, respect_gitignore = true })
+      end, { desc = 'Find files' })
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Live grep' })
       vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Buffers' })
     end,
