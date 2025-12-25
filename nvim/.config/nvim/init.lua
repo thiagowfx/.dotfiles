7e49be6667c3fc0f7f7d99bdcc1c838ebc6a6d0a
Thiago Perrotta <thiago@perrotta.dev>
2025-12-25 04:23:33 -0300

    nvim: add ruff

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -250,10 +250,11 @@ local plugins = {
     'mfussenegger/nvim-lint',
     config = function()
       require('lint').linters_by_ft = {
-        sh = { 'shellcheck' },
         bash = { 'shellcheck' },
-        yaml = { 'yamllint' },
         json = { 'jsonlint' },
+        python = { 'ruff' },
+        sh = { 'shellcheck' },
+        yaml = { 'yamllint' },
       }
       vim.api.nvim_create_autocmd({ 'BufWritePost', 'BufReadPost' }, {
         callback = function() require('lint').try_lint() end,
