86a87eeb435524ef31c2d3b2103036138a20b78d
Thiago Perrotta <thiago@perrotta.dev>
2025-12-27 01:06:11 -0300

    move zsh prompt config to .zshrc.local

--- zsh/.zshrc.d/prompt.zsh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-# Set zsh prompt.
-
-# Customize grml prompt.
-#   Two lines instead of one
-zstyle ':prompt:grml:left:items:user' pre '%B%F{green}'
-zstyle ':prompt:grml:left:setup' items rc change-root user at host path vcs newline percent
