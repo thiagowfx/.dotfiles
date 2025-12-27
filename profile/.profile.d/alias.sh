a0756d58749f43b14206c0756d88b07ab2ce7206
Thiago Perrotta <thiago@perrotta.dev>
2025-12-27 01:00:14 -0300

    consolidate profile env vars and functions into .profilerc

--- profile/.profile.d/alias.sh
+++ profile/.profile.d/alias.sh
@@ -11,31 +11,28 @@ alias la="ls -la --color=auto"
 alias l="ls -l --color=auto"
 alias ll="l"
 
-# https://direnv.net/
-if hash direnv >/dev/null 2>&1; then
-	alias de="direnv edit || direnv edit ."
-fi
-
 # eza: modern drop-in replacement for ls
 if hash eza >/dev/null 2>&1; then
-	alias ls="eza -F --group-directories-first"
+        alias ls="eza -F --group-directories-first"
 fi
 
-# misspellings for git
+# https://direnv.net/
+if hash direnv >/dev/null 2>&1; then
+        alias de="direnv edit || direnv edit ."
+fi
+
+# misspellings
 alias t=git
 alias it=git
 alias gi=git
 alias gt=git
 alias gti=git
+alias sl=ls
 
 alias cdg='cd "$(git root)"'
 
-# more misspellings
-alias sl=ls
-
-# shorts
+# shortcuts
 alias k=kubectl
-alias tf=terraform
 
 # muscle memory
 alias unstow="stow -D"
