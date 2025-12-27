5f3f36ac64606a917d32a46d1bd87b2e1ce8e7d9
Thiago Perrotta <thiago@perrotta.dev>
2025-12-27 00:49:58 -0300

    merge functions.sh into .profilerc

--- profile/.profile.d/functions.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Inspiration from https://frantic.im/cdtmp/ and grml-zsh-config's cdt
-# Usage: cdtmp [foo]
-# Creates temp dir with user, date, and optional prefix
-cdtmp() {
-	builtin cd "$(mktemp -d -t "$USER-${1:+$1-}$(date +%Y-%m-%d)-XXXXXX")" || return
-	builtin pwd
-}
