00f44b91530734fd7b461fa627b617eed54d4afc
Thiago Perrotta <thiago@perrotta.dev>
2026-05-26 12:51:44 +0200

    zsh plug-in order

--- zsh/.zshrc.d/plugins.zsh
+++ zsh/.zshrc.d/plugins.zsh
@@ -1,13 +1,7 @@
 #!/bin/zsh
 
-# Fish shell-like syntax highlighting for Zsh.
-src_files "$HOME/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
-
-# Fish shell-like autosuggestions for zsh.
-# Suggest commands as you type based on history and completions.
-#   Use C-e or C-f to accept a suggestion.
-src_files "$HOME/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh"
-ZSH_AUTOSUGGEST_STRATEGY=(history completion)
+# atuin: https://docs.atuin.sh/
+(( $+commands[atuin] )) && eval "$(atuin init zsh --disable-up-arrow)"
 
 # homebrew
 (( $+commands[brew] )) && FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
@@ -16,16 +10,24 @@ ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 # https://direnv.net/
 (( $+commands[direnv] )) && eval "$(direnv hook zsh)"
 
-# a smarter cd command - z
-# https://github.com/ajeetdsouza/zoxide
-(( $+commands[zoxide] )) && eval "$(zoxide init zsh)"
-
 # fzf: fuzzy file finder
 # https://github.com/junegunn/fzf#setting-up-shell-integration
 (( $+commands[fzf] )) && source <(fzf --zsh)
 
-# atuin: https://docs.atuin.sh/
-(( $+commands[atuin] )) && eval "$(atuin init zsh --disable-up-arrow)"
-
 # worktrunk: https://worktrunk.dev/
 (( $+commands[wt] )) && eval "$(wt config shell init zsh)"
+
+# a smarter cd command — z
+# https://github.com/ajeetdsouza/zoxide
+(( $+commands[zoxide] )) && eval "$(zoxide init zsh)"
+
+# Fish shell-like autosuggestions for zsh.
+# Suggest commands as you type based on history and completions.
+#   Use C-e or C-f to accept a suggestion.
+src_files "$HOME/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh"
+ZSH_AUTOSUGGEST_STRATEGY=(history completion)
+
+# Fish shell-like syntax highlighting for Zsh.
+# MUST be sourced last — wraps ZLE widgets and breaks if later plugins
+# (fzf, atuin, etc.) redefine the widgets it wraps.
+src_files "$HOME/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
