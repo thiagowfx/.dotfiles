0b0dfce38bd127bfa95a6746bedd2878ce82e7a2
Thiago Perrotta <thiago@perrotta.dev>
2026-08-21 02:08:45 +0200

    mise: rename profile script

--- zsh/.zshrc.d/plugins.zsh
+++ zsh/.zshrc.d/plugins.zsh
@@ -9,7 +9,7 @@
 
 # mise: tool versions, env vars and tasks
 # https://mise.jdx.dev/dev-tools/shims.html#how-to-add-mise-shims-to-path
-# PATH activation for interactive shells; .profile.d/programming.sh sets up the
+# PATH activation for interactive shells; .profile.d/mise.sh sets up the
 # shims that non-interactive shells use.
 (( $+commands[mise] )) && eval "$(mise activate zsh)"
 
